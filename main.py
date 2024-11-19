204d9190a762f791f8052e4090315157f30834ba
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-19 19:48:40 +0500

    weather

--- main.py
+++ main.py
@@ -25,3 +25,15 @@ if (palind(r)):
     print("the tuple is flip flop")
 else:
     print("the tuple is not flip flop")
+weather=(1,0,0,0,1,1,0)
+sunny=0
+rainy=0
+for i in range(0,7):
+    if(weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
+if(sunny>rainy):
+    print("good weather")
+else:
+    print("bad weather")
