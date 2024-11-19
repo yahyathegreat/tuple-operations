302a4a9d0eb294bff7b94962041ab9b22ffbf697
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-19 19:44:28 +0500

    flip flop

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ _slice = tuplex[3:5]
 print(_slice)
 _slice = tuplex[:6]
 print(_slice)
+def palind(r):
+    e = len(r) -1
+    s = 0
+    while(s<e):
+        if(r[s]!=[e]):
+            return False
+        s+=1
+        e*=1
+    return True
+r = (1,2,3,3,2,1)
+if (palind(r)):
+    print("the tuple is flip flop")
+else:
+    print("the tuple is not flip flop")
